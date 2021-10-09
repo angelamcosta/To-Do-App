51e6c412b8eb7329a925252e5705c4c7fb40439b
Angela Lima <angelamcosta_@hotmail.com>
2021-10-09 09:09:15 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# To-Do App
+
+To-Do App is a HTML single page basic to-do app that enables a user to add daily tasks.
+
+## Objective
+
+The to-do web app allow users to add new items to a to-do list. Also, a user can mark a task “complete” upon its successful completion, and these completed tasks appear on a separate list. The app uses local storage to allow users to resume the app upon restarting, right from where they left it off the last time.
+
+## Built With
+* Javascript
+* JQuery
+* Bootstrap 5
+
+## Planned
+* Add the date and time when a task is added and/or completed.
---
+++ assets/js/site.js
@@ -0,0 +1,90 @@
+function addItem() {
+    var item = $("#newItem").val();
+    $("#newItem").val("");
+
+    var items = JSON.parse(localStorage.getItem("toDoList"));
+    if (items === null) {
+        items = [];
+    };
+    items.push(item);
+    localStorage.setItem("toDoList", JSON.stringify(items));
+
+    refreshLists();
+};
+
+function getItems() {
+    var items = JSON.parse(localStorage.getItem("toDoList"));
+
+    if (items === null || items.length === 0) {
+        $("#toDoList").append("<span class='text-muted'>Start by adding a few tasks</span>");
+    } else {
+        for (var i = 0; i < items.length; i++) {
+            var itemId = "item_" + i;
+            $("#toDoList").append("<div class='m-3'> <input id='" + itemId + "' type='checkbox' onchange='statusChange(this.id);'/>&#160; <label for='" + itemId + "'>" + items[i] + "</label> <button class='btn btn-link btn-sm' id='" + itemId + "' onclick='deleteItem(this.id);'><img src='/assets/icons/trash-can.png' width='25' height='25' alt='trash logo'></button> <br> </div>");
+        }
+    };
+    
+    var itemsDone = JSON.parse(localStorage.getItem("doneList"));
+
+    if (itemsDone === null || itemsDone.length === 0) {
+        $("#doneList").append("<span class='text-muted'>You haven't finished any tasks yet</span>");
+    } else {
+        for (var i = 0; i < itemsDone.length; i++) {
+            var itemId = "item_" + i;
+            $("#doneList").append("<div class='m-3'> <input id='" + itemId + "' type='checkbox' onchange='statusChange(this.id);' checked/>&#160; <label for='" + itemId + "' class='text-decoration-line-through'>" + itemsDone[i] + "</label> <br> </div>");
+        }
+    }
+};
+
+function statusChange(itemId) {
+    if ($('#' + itemId).is(":checked")) {
+        var itemId = itemId.split('_');
+        var items = JSON.parse(localStorage.getItem("toDoList"));
+        var itemsDone = JSON.parse(localStorage.getItem("doneList"));
+
+        if (itemsDone === null) {
+            itemsDone = [];
+        };
+
+        itemsDone.push(items[itemId[1]]);
+        localStorage.setItem("doneList", JSON.stringify(itemsDone));
+
+        items.splice(itemId[1], 1);
+        localStorage.setItem("toDoList", JSON.stringify(items));
+    } else {
+        var itemId = itemId.split('_');
+        var items = JSON.parse(localStorage.getItem("toDoList"));
+        var itemsDone = JSON.parse(localStorage.getItem("doneList"));
+
+        if (items === null) {
+            items = [];
+        };
+
+        items.push(itemsDone[itemId[1]]);
+        localStorage.setItem("toDoList", JSON.stringify(items));
+
+        itemsDone.splice(itemId[1], 1);
+        localStorage.setItem("doneList", JSON.stringify(itemsDone));
+    };
+
+    refreshLists();
+};
+
+function deleteItem(itemId) {
+    var itemId = itemId.split('_');
+    var items = JSON.parse(localStorage.getItem("toDoList"));
+    
+    items.splice(itemId[1], 1);
+    localStorage.setItem("toDoList", JSON.stringify(items));
+
+    refreshLists();
+};
+
+function refreshLists() {
+    $("#toDoList").empty();
+    $("#doneList").empty();
+
+    getItems();
+};
+
+getItems();

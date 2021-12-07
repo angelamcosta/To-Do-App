dc82a611ac34bc84d1134dc2e7a34f0c47252592
angelamcosta <angelamcostalima@icloud.com>
2021-12-07 21:40:27 +0000

    Fix code smells

--- js/to-do.js
+++ js/to-do.js
@@ -5,46 +5,51 @@ function addItem() {
     var items = JSON.parse(localStorage.getItem("toDoList"));
     if (items === null) {
         items = [];
-    };
+    }
     items.push(item);
     localStorage.setItem("toDoList", JSON.stringify(items));
 
     refreshLists();
-};
+}
 
 function getItems() {
+    var itemId;
+
     var items = JSON.parse(localStorage.getItem("toDoList"));
 
     if (items === null || items.length === 0) {
         $("#toDoList").append("<span class='text-muted'>Start by adding a few tasks</span>");
     } else {
-        for (var i = 0; i < items.length; i++) {
-            var itemId = "item_" + i;
+        for (i = 0; i < items.length; i++) {
+            itemId = "item_" + i;
             $("#toDoList").append("<div class='m-3'> <input id='" + itemId + "' type='checkbox' onchange='statusChange(this.id);'/>&#160; <label for='" + itemId + "'>" + items[i] + "</label> <button class='btn btn-link btn-sm' id='" + itemId + "' onclick='deleteItem(this.id);'><img src='icons/trash-can.png' width='25' height='25' alt='trash logo'></button> <br> </div>");
         }
-    };
+    }
     
     var itemsDone = JSON.parse(localStorage.getItem("doneList"));
 
     if (itemsDone === null || itemsDone.length === 0) {
         $("#doneList").append("<span class='text-muted'>You haven't finished any tasks yet</span>");
     } else {
-        for (var i = 0; i < itemsDone.length; i++) {
-            var itemId = "item_" + i;
-            $("#doneList").append("<div class='m-3'> <input id='" + itemId + "' type='checkbox' onchange='statusChange(this.id);' checked/>&#160; <label for='" + itemId + "' class='text-decoration-line-through'>" + itemsDone[i] + "</label> <br> </div>");
+        for (var j = 0; j < itemsDone.length; j++) {
+            itemId = "item_" + j;
+            $("#doneList").append("<div class='m-3'> <input id='" + itemId + "' type='checkbox' onchange='statusChange(this.id);' checked/>&#160; <label for='" + itemId + "' class='text-decoration-line-through'>" + itemsDone[j] + "</label> <br> </div>");
         }
     }
-};
+}
 
 function statusChange(itemId) {
+    var items;
+    var itemsDone;
+
     if ($('#' + itemId).is(":checked")) {
-        var itemId = itemId.split('_');
-        var items = JSON.parse(localStorage.getItem("toDoList"));
-        var itemsDone = JSON.parse(localStorage.getItem("doneList"));
+        itemId = itemId.split('_');
+        items = JSON.parse(localStorage.getItem("toDoList"));
+        itemsDone = JSON.parse(localStorage.getItem("doneList"));
 
         if (itemsDone === null) {
             itemsDone = [];
-        };
+        }
 
         itemsDone.push(items[itemId[1]]);
         localStorage.setItem("doneList", JSON.stringify(itemsDone));
@@ -52,39 +57,39 @@ function statusChange(itemId) {
         items.splice(itemId[1], 1);
         localStorage.setItem("toDoList", JSON.stringify(items));
     } else {
-        var itemId = itemId.split('_');
-        var items = JSON.parse(localStorage.getItem("toDoList"));
-        var itemsDone = JSON.parse(localStorage.getItem("doneList"));
+        itemId = itemId.split('_');
+        items = JSON.parse(localStorage.getItem("toDoList"));
+        itemsDone = JSON.parse(localStorage.getItem("doneList"));
 
         if (items === null) {
             items = [];
-        };
+        }
 
         items.push(itemsDone[itemId[1]]);
         localStorage.setItem("toDoList", JSON.stringify(items));
 
         itemsDone.splice(itemId[1], 1);
         localStorage.setItem("doneList", JSON.stringify(itemsDone));
-    };
+    }
 
     refreshLists();
-};
+}
 
 function deleteItem(itemId) {
-    var itemId = itemId.split('_');
+    itemId = itemId.split('_');
     var items = JSON.parse(localStorage.getItem("toDoList"));
     
     items.splice(itemId[1], 1);
     localStorage.setItem("toDoList", JSON.stringify(items));
 
     refreshLists();
-};
+}
 
 function refreshLists() {
     $("#toDoList").empty();
     $("#doneList").empty();
 
     getItems();
-};
+}
 
 getItems();
